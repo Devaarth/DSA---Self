d6cec9bbeee5e1827156b997189528432b47d7cb
Devaarth <devaarth@gmail.com>
2026-04-27 21:19:13 +0530

    Time: 1 ms (85.86%) | Memory: 55.3 MB (41.68%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let newMap = new Map();
+    for(let i=0;i<nums.length;i++){
+        let item = target-nums[i];
+        if(newMap.has(item)){  
+                return [newMap.get(item),i]
+        }
+            newMap.set(nums[i],i)
+        }
+      return []  
+    }
